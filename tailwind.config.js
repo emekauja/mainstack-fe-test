1cad4c85d8fbbd5bfafbd163ff175fcded55807b
emekauja <ujadninth@gmail.com>
2024-04-09 10:38:54 +0100

    added gray color to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,24 @@ export default {
       degularDisplay: ['Degular Display', 'sans-serif'],
 
     },
+    colors: {
+      white: '#ffffff',
+      gray: {
+        50: '#EFF1F6',
+        100:  '#DBDEE5',
+        200: '#D9D9D9',
+        300: '#888F95',
+        400: '#56616B',
+        DEFAULT: '#56616B',
+      },
+      black: {
+        // 50: ,
+        // 100: ,
+        // 200: ,
+        300: '#131316',
+        DEFAULT: '#131316',
+      }
+    },
     extend: {},
   },
   plugins: [
