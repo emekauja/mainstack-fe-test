bbac1da6b25973c77a52669ff30e86a63e60f4ff
emekauja <ujadninth@gmail.com>
2024-04-10 18:46:11 +0100

    fix: avatar size props

--- src/components/primitive/avatar/avatar.tsx
+++ src/components/primitive/avatar/avatar.tsx
@@ -9,7 +9,11 @@ interface IAvatarProps {
 export const Avatar = ({ name, size = '32' }: IAvatarProps) => {
   return (
     <div
-      className={`rounded-full w-[${size}px] h-[${size}px] text-white grid place-items-center bg-gradient-to-r from-black-50 to-black-300 cursor-pointer`}
+      className={`rounded-full text-white grid place-items-center bg-gradient-to-r from-black-50 to-black-300 cursor-pointer`}
+      style={{
+        height: `${size}px`,
+        width: `${size}px`,
+      }}
     >
       <Text>{getNameInitials({ name })}</Text>
     </div>
