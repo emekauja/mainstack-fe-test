8f690a943923ca3a816208e1eebf22ad29d49f28
emekauja <ujadninth@gmail.com>
2024-04-11 12:08:56 +0100

    fix: sidebar and header styling

--- src/components/connected/header/header.tsx
+++ src/components/connected/header/header.tsx
@@ -55,22 +55,24 @@ export const Header = ({ user }: IHeaderProps) => {
     );
   }
   return (
-    <div className="relative flex items-center justify-between">
-      <a href="">
-        <MainStackLogo />
-      </a>
+    <div className="sticky top-0 block pt-4 pb-3 px-4 z-50 bg-white">
+      <div className="relative flex items-center justify-between shadow-sec rounded-full border-2 border-white h-16 pr-2 pl-6 space-x-3">
+        <a href="">
+          <MainStackLogo />
+        </a>
 
-      <div>
-        <Menu />
-      </div>
+        <div>
+          <Menu />
+        </div>
 
-      <div className="flex items-center space-x-4">
-        <MenuButton icon={<BellIcon />} variant="icon" />
-        <MenuButton icon={<MessageIcon />} variant="icon" />
+        <div className="flex items-center space-x-2">
+          <MenuButton icon={<BellIcon />} variant="icon" />
+          <MenuButton icon={<MessageIcon />} variant="icon" />
 
-        <div className="rounded-full space-x-2 flex items-center py-1 pr-3 pl-[5px] bg-gray-50">
-          <Avatar name={user?.name} />
-          <MenuIcon />
+          <div className="rounded-full space-x-2 flex items-center py-1 pr-3 pl-[5px] bg-gray-50">
+            <Avatar name={user?.name} />
+            <MenuIcon />
+          </div>
         </div>
       </div>
     </div>
--- src/components/connected/sidebar/sidebar.tsx
+++ src/components/connected/sidebar/sidebar.tsx
@@ -1,4 +1,6 @@
-import React, { ReactNode } from 'react';
+import React, { Component, ReactNode } from 'react';
+import { Tooltip, withTooltip } from 'react-tippy';
+import { MainTooltip } from '../../primitive/tooltip/tooltip';
 
 interface ISidebarProps {}
 
@@ -22,19 +24,27 @@ const sidebarItems = [
 ];
 
 export const Sidebar = ({}: ISidebarProps) => {
-  function SidebarItem({ icon, tooltip }: { icon?: string; tooltip?: string }) {
+  function SidebarItem({
+    icon,
+    tooltip,
+  }: {
+    icon?: string;
+    tooltip: string | undefined;
+  }) {
     return (
-      <div className="group flex items-center justify-center w-max p-2 max-h-[40px] rounded-full hover:bg-gray-50">
-        <img
-          src={`/src/assets/icons/${icon}.svg`}
-          alt={tooltip ? tooltip : ''}
-          className="w-6 h-6 grayscale group-hover:grayscale-0"
-        />
+      <div className="group relative flex items-center justify-center w-max p-2 max-h-[40px] rounded-full hover:bg-gray-50 cursor-pointer">
+        <MainTooltip title={tooltip}>
+          <img
+            src={`/src/assets/icons/${icon}.svg`}
+            alt={tooltip ? tooltip : ''}
+            className="w-6 h-6 grayscale group-hover:grayscale-0"
+          />
+        </MainTooltip>
       </div>
     );
   }
   return (
-    <div className="flex flex-col gap-1 shadow-main w-max p-1 rounded-full">
+    <div className="fixed left-4 top-56 flex flex-col gap-1 shadow-main w-max p-1 rounded-full">
       {sidebarItems.map((item, idx) => (
         <SidebarItem key={idx} icon={item.icon} tooltip={item.tooltip} />
       ))}
