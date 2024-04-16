f780b792490a0068c12ed93109f2acba814d63a2
emekauja <ujadninth@gmail.com>
2024-04-17 00:26:05 +0100

    fix: make chart dynamic by extending props

--- src/components/primitive/revenue/chart.tsx
+++ src/components/primitive/revenue/chart.tsx
@@ -4,54 +4,15 @@ interface IChartProps {
   data?: [];
   startDate?: Date;
   endDate?: Date;
+  dataKey: string;
 }
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-export const RevenueChart = ({ startDate, endDate }: IChartProps) => {
+export const RevenueChart = ({
+  data,
+  dataKey,
+  startDate,
+  endDate,
+}: IChartProps) => {
   // const formatDate =  new Date("2022/5/19 GMT");
   return (
     <div className="h-[308px]">
@@ -64,7 +25,7 @@ export const RevenueChart = ({ startDate, endDate }: IChartProps) => {
         <LineChart width={728} height={100} data={data}>
           <Line
             type="monotone"
-            dataKey="uv"
+            dataKey={dataKey}
             stroke="#FF5403"
             activeDot={false}
           />
