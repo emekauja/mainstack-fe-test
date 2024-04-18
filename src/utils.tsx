98b9ad9619ca94d45cb51c2e815adf1f4658ecc3
emekauja <ujadninth@gmail.com>
2024-04-18 13:53:58 +0100

    refactor: utils to contain jsx elements

---
+++ src/utils.tsx
@@ -0,0 +1,113 @@
+import {
+  GroupIcon,
+  HomeIcon,
+  HomeIconActive,
+  InsertChartIcon,
+  // InsertChartIconActive,
+  PaymentsIcon,
+  PaymentsIconActive,
+  WidgetsIcon,
+  // WidgetsIconActive,
+} from './assets/icons/icons';
+
+interface INameInitialsProps {
+  name: string | undefined;
+  maxInitials?: number;
+}
+
+export type RoutesType = 'revenue' | 'home' | 'apps' | 'CRM' | 'analytics';
+
+export type IconKeys = 'icon' | 'iconActive';
+
+export const getNameInitials = ({
+  name,
+  maxInitials = 2,
+}: INameInitialsProps) => {
+  if (!name) return '';
+  return name
+    .split(/\s/)
+    .map((part) => part.substring(0, 1).toUpperCase())
+    .filter((value) => !!value)
+    .slice(0, maxInitials)
+    .join('')
+    .toUpperCase();
+};
+
+export const transactionTypeOptions = [
+  { label: 'Charge backs', value: 'charge-backs' },
+  { label: 'Withdraw', value: 'withdraw' },
+  { label: 'Deposit', value: 'deposit' },
+];
+
+export const defaultDates = [
+  'Today',
+  'Last 7 days',
+  'This month',
+  'Last 3 months',
+  'This year',
+  'Last year',
+  'All time',
+];
+
+export const navData: Record<RoutesType, string> = {
+  home: 'Home',
+  analytics: 'Analytics',
+  revenue: 'Revenue',
+  CRM: 'CRM',
+  apps: 'Apps',
+};
+
+export function renderIcon(
+  type: RoutesType
+): Record<IconKeys, React.ReactElement> | undefined {
+  switch (type) {
+    case 'home':
+      return {
+        icon: <HomeIcon />,
+        iconActive: <HomeIconActive />,
+      };
+    case 'analytics':
+      return {
+        icon: <InsertChartIcon />,
+        iconActive: <InsertChartIcon />,
+        // iconActive: <InsertChartIconActive />,
+      };
+    case 'revenue':
+      return {
+        iconActive: <PaymentsIconActive />,
+        icon: <PaymentsIcon />,
+      };
+    case 'CRM':
+      return {
+        icon: <GroupIcon />,
+        iconActive: <GroupIcon />,
+      };
+    case 'apps':
+      return {
+        icon: <WidgetsIcon />,
+        iconActive: <WidgetsIcon />,
+        // iconActive: <WidgetsIconActive />,
+      };
+    default:
+      return undefined;
+  }
+}
+
+export const sidebarItems = [
+  {
+    icon: 'link',
+    tooltip: 'Link in Bio',
+  },
+  {
+    icon: 'store',
+    tooltip: 'Store',
+  },
+  {
+    icon: 'media-kit',
+    tooltip: 'Media Kit',
+  },
+  {
+    icon: 'invoicing',
+    tooltip: 'Invoicing',
+  },
+];
